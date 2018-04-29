52c2167711b8912d6374de1ae61a6878e4a525e2
Andika Ahmad Ramadhan <aspendaka@gmail.com>
2018-04-29 20:32:13 +0700

    menambahkan animasi scroll bawah dan atas

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,9 @@ gulp.task('libs', () => {
             './src/js/jquery.js',
             './src/js/bootstrap.min.js',
             './src/js/owl.carousel.js',
-            './src/js/script.js',            
+            './src/js/anime.min.js',
+            './src/js/prism.js',
+            './src/js/script.js',
         ])
         .pipe(concat('libs.js'))
         .pipe(uglify())
